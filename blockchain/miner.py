c93fd267a288020a5a9a7699eb8dc04299cfa502
Nabeelah <noblenab13@gmail.com>
2019-11-01 12:04:40 +0100

    Solve blockchain

--- blockchain/miner.py
+++ blockchain/miner.py
@@ -25,7 +25,12 @@ def proof_of_work(last_proof):
 
     print("Searching for next proof")
     proof = 0
+    user_input = f'{last_proof}'.encode()
+    user_hash = hashlib.sha256(user_input).hexdigest()
     #  TODO: Your code here
+    while valid_proof(user_hash, proof) is False:
+        proof += 1
+    return proof
 
     print("Proof found: " + str(proof) + " in " + str(timer() - start))
     return proof
@@ -38,9 +43,11 @@ def valid_proof(last_hash, proof):
 
     IE:  last_hash: ...AE9123456, new hash 123456888...
     """
+    user_input = f'{proof}'.encode()
+    hash = hashlib.sha256(user_input).hexdigest()
 
     # TODO: Your code here!
-    pass
+    return hash[:6] == last_hash[-6:]
 
 
 if __name__ == '__main__':
@@ -68,8 +75,7 @@ if __name__ == '__main__':
         data = r.json()
         new_proof = proof_of_work(data.get('proof'))
 
-        post_data = {"proof": new_proof,
-                     "id": id}
+        post_data = {"proof": new_proof, "id": id}
 
         r = requests.post(url=node + "/mine", json=post_data)
         data = r.json()
