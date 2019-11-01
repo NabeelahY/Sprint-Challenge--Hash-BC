8381d93b9b857dad08b8f00a74d4ee4c6840c11b
Nabeelah <noblenab13@gmail.com>
2019-11-01 10:28:08 +0100

    Solve Merging Two Packages problem

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,20 +1,27 @@
 #  Hint:  You may not need all of these.  Remove the unused functions.
-from hashtables import (HashTable,
-                        hash_table_insert,
-                        hash_table_remove,
-                        hash_table_retrieve,
-                        hash_table_resize)
+from hashtables import (HashTable, hash_table_insert)
 
 
 def get_indices_of_item_weights(weights, length, limit):
-    ht = HashTable(16)
+    ht = HashTable(length)
 
-    """
-    YOUR CODE HERE
-    """
+    arr_len = 0
+    arr = []
 
-    return None
+    while arr_len < length:
+        diff = limit - weights[arr_len]
+        for i in range(length):
+            hash_table_insert(ht, weights[i], i)
+            if diff == weights[i] and arr_len != i:
+                arr.append(i)
+       
+        arr_len += 1
 
+    if len(arr) == 0:
+        return None
+    return arr
+
+    print(arr) 
 
 def print_answer(answer):
     if answer is not None:
